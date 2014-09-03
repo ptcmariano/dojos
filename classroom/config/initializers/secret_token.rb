4737232fbfe51228626bd72171532e2b0c975a1f
paulotiagocm <paulotiagocm@hotmail.com>
2014-09-03 16:26:59 -0300

    Configurado app e adicionado scaffold de curso

---
+++ classroom/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Classroom::Application.config.secret_token = '78efd0588231731da18f37a8b82827fe033209e01399208bcdbcf56a9e00323ace5bfefbe4b11a83b70d6a5fa2f555ea3341d6621bec1e98637aeea4bd1c7fa3'
---
+++ classroom/spec/factories/courses.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :course do
+    name "MyString"
+    start_date "2014-09-03"
+    end_date "2014-09-03"
+  end
+end
